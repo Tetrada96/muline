4a0cb97c8f998a4ba5a215949a71bd7bb0e58b44
Irina Vorobyeva <isvorobyeva@mail.ru>
2025-01-11 01:07:55 +0300

    fix auth

--- CheckMuline_front/src/App.tsx
+++ CheckMuline_front/src/App.tsx
@@ -14,8 +14,38 @@ import { Menu } from './fragments/Menu/Menu';
 import { FlexBlock } from './components/FlexBlock/FlexBlock';
 import { getUser } from './services/users';
 
+import * as VKID from '@vkid/sdk';
+
 function App() {
   const navigate = useNavigate();
+
+  const container = document.getElementById('VkIdSdkOneTap');
+
+  VKID.Config.init({
+  app:  52910357, // Идентификатор приложения.
+  redirectUrl: 'https://mulinehub.ru', // Адрес для перехода после авторизации.
+  state: crypto.randomUUID(), // Произвольная строка состояния приложения.
+  codeVerifier: crypto.randomUUID(), // Параметр в виде случайной строки. Обеспечивает защиту передаваемых данных.
+  scope: 'email phone', // Список прав доступа, которые нужны приложению.
+});
+
+const oneTap = new VKID.OneTap();
+
+if (container) {
+  // Отрисовка кнопки в контейнере с именем приложения APP_NAME, светлой темой и на русском языке.
+  oneTap.render({ container: container, scheme: VKID.Scheme.LIGHT, lang: VKID.Languages.RUS })
+        .on(VKID.WidgetEvents.ERROR, (e: any) => {console.log(e)}); // handleError — какой-либо обработчик ошибки.
+}
+  
+
+  oneTap.on(VKID.OneTapInternalEvents.LOGIN_SUCCESS, function (payload: {code: string, device_id: string}) {
+    const code = payload.code;
+    const deviceId = payload.device_id;
+  
+    VKID.Auth.exchangeCode(code, deviceId)
+      .then((res) => console.log(res))
+      .catch((e) => console.log(e));
+  });
 
   const { store } = useContext(Context);
 
--- CheckMuline_front/src/fragments/Header/Header.tsx
+++ CheckMuline_front/src/fragments/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import { observer } from 'mobx-react-lite';
-import * as VKID from '@vkid/sdk';
+
 
 import styles from './Header.module.scss';
 import { FlexBlock } from '../../components/FlexBlock/FlexBlock';
@@ -9,39 +9,11 @@ import { Context } from '../..';
 import { Button } from '../../components/Button/Button';
 import { IconButton } from '../../components/IconButton/IconButton';
 
-VKID.Config.init({
-  app:  51778213, // Идентификатор приложения.
-  redirectUrl: '/colors/', // Адрес для перехода после авторизации.
-  state: crypto.randomUUID(), // Произвольная строка состояния приложения.
-  codeVerifier: crypto.randomUUID(), // Параметр в виде случайной строки. Обеспечивает защиту передаваемых данных.
-  scope: 'email phone', // Список прав доступа, которые нужны приложению.
-});
+
+
 
 export const Header = observer(({ children, hideMenu }: { children?: JSX.Element | null; hideMenu: boolean }) => {
   const { store } = useContext(Context);
-
-  const oneTap = new VKID.OneTap();
-
-// Получение контейнера из разметки.
-  const container = document.getElementById('VkIdSdkOneTap');
-  
-  console.log(container)
-
-// Проверка наличия кнопки в разметке.
-if (container) {
-  // Отрисовка кнопки в контейнере с именем приложения APP_NAME, светлой темой и на русском языке.
-  oneTap.render({ container: container, scheme: VKID.Scheme.LIGHT, lang: VKID.Languages.RUS })
-        .on(VKID.WidgetEvents.ERROR, (e: any) => {console.log(e)}); // handleError — какой-либо обработчик ошибки.
-}
-  
-oneTap.on(VKID.OneTapInternalEvents.LOGIN_SUCCESS, function (payload: {code: string, device_id: string}) {
-  const code = payload.code;
-  const deviceId = payload.device_id;
-
-  VKID.Auth.exchangeCode(code, deviceId)
-    .then((res) => console.log(res))
-    .catch((e) => console.log(e));
-});
 
   return (
     <FlexBlock className={styles.headerWrapper}>
