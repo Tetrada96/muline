f05240a36f60e7b32633bd423c7230b8c5c76953
Vorobeva Irina <isvorobyeva@mail.ru>
2025-01-11 20:47:07 +0300

    stage 15

--- CheckMuline_front/src/pages/LoginPage.tsx
+++ CheckMuline_front/src/pages/LoginPage.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { observer } from 'mobx-react-lite';
 
 import { FlexBlock } from '../components/FlexBlock/FlexBlock';
 import styles from './styles.module.scss';
+import { Context } from '..';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginPage = observer(() => {
+
+  const { store } = useContext(Context);
+
+  const navigate = useNavigate();
+  if (store.user) {
+    navigate('/colors')
+  }
 
 
   return (
